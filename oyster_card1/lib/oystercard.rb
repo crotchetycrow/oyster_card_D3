aea8c7159cc85cdcad4b0498d5822783ea7017b3
Yates101 <alecyates94@gmail.com>
2021-03-09 14:02:12 +0000

    step 6 commit

---
+++ oyster_card1/lib/oystercard.rb
@@ -0,0 +1,16 @@
+class Oystercard
+  attr_reader :balance
+  DEFAULT_BALANCE = 0
+  # MAXIMUM_BALANCE = 90
+
+  def initialize(balance = DEFAULT_BALANCE)
+    @balance = balance
+  end
+
+  def top_up(amount)
+    if @balance + amount > 90
+      raise "Top up invalid, balance maximum is £90, your current balance is #{@balance}"
+    end
+    @balance += amount
+  end
+end
---
+++ oyster_card1/spec/oystercard_spec.rb
@@ -0,0 +1,23 @@
+require 'oystercard'
+
+describe Oystercard do
+  describe 'when first created' do
+    it { is_expected.to respond_to(:balance) }
+
+    it 'initialises with a blanace of zero' do
+      expect(subject.balance).to eq 0
+    end
+  end
+
+  describe 'top up features' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+
+    it 'increases the balance by the argument amount using :top_up' do
+      expect { subject.top_up(10) }.to change { subject.balance }.by(10)
+    end
+
+    it 'raises an error if the top_up method puts balance over £90' do
+      expect { subject.top_up(100) }.to raise_error("Top up invalid, balance maximum is £90, your current balance is #{subject.balance}")
+    end
+  end
+end
